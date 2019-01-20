000af00d7f72e5dfd5a0a48fc707caba3de67606
interestor <tail6182@gmail.com>
2019-01-20 23:07:22 +0900

    Add serviceworker.js
    PWA

---
+++ serviceworker.js
@@ -0,0 +1,45 @@
+//https://techblog.asahi-net.co.jp/entry/2018/07/06/145931より
+
+//キャッシュ名
+var cacheName = 'nomoredame';
+
+//キャッシュしたいファイルのリスト(今回は3種類。HTML本体、jsファイル、CSSファイルが対象)
+var filesToCache = [
+   'index.html',
+   // 'jquery.js',
+   'style.css',
+];
+
+//ブラウザへのインストール時にキャシュしたいファイルを追加する
+self.addEventListener('install',function(event){
+  event.waitUntil(
+    caches.open(cacheName).then(function(cache){
+      return cache.addAll(filesToCache);
+    })
+  );
+});
+
+
+//ServiceWorkerが有効になるときcacheNameがちがうキャッシュを削除する
+self.addEventListener('activate',function(event){
+  event.waitUntil(
+    caches.keys().then(function(keyList){
+      return Promise.all(keyList.map(function(key){
+        if(key !== cacheName){
+          return caches.delete(key);
+        }
+      }));
+    })
+  );
+  return self.clients.claim();
+});
+
+
+//キャッシュがあれば呼び出し、ない場合ネットワークから取りに行く
+self.addEventListener('fetch',function(event){
+  event.respondWith(
+    caches.match(event.request).then(function(response){
+      return response || fetch(event.request);
+    })
+  );
+});
